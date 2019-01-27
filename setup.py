602d0def773b110a71e14d1bbd2aca945d623a32
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-01-27 19:16:33 +0200

    Organised folder structure and refactored some code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='turing',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='b1zon',
+    author_email='kolausf@gmail.com',
+    description='Turing machine simulator with some macrosses'
+    )
