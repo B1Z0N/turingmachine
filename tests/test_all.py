6600fcca5366313733c29a03cf1a8243080a835b
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-01-28 20:26:29 +0200

    Added from_str and from_file alternative constructors. Commented some code with docstrings.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,13 +1,7 @@
-from turing import machine, macro, gui
+from turingmachine.machine import  TuringMachine as TM
 
-mt = machine.TuringMachine([1, 2, 3], 'q1')
-mt.rule_str('1 q1 -> 1 q1 R')
-mt.rule_str('2 q1 -> 1 q1 L')
-mt.rule_str('3 q1 -> 2 q1 L')
-mt.rule_str('DEL q1 -> 1 q1 S')
-
-while True:
-    input()
-    mt.move()
-    print(mt)
+tm = TM.from_str('1,2,3:0::q1:1 q1 -> 2 q3 R,3 q2 -> 4 q4 S')
+print(tm)
+tm = TM.from_file('testf')
+print(tm)
 
