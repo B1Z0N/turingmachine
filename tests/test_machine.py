8dbfad184336be7311aad15b4f8240029c09407d
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-01-31 23:41:52 +0200

    1. Added central element and __getitem__ and __setitem__ in accordance to center. 2. Rewritten some test and refactored code 3. Started designing and coding macros 4. Added exceptions to macros module

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -78,6 +78,29 @@ class TestTuringMachine:
     def test_stop(self):
         self.tm_for_move.stop('stop')
 
+    def test__prepare_index(self):
+        tm = TuringMachine.from_str('1,2,3:::q1:')
+        tm._center = 1
+
+        print(tm)
+
+        index1 = tm._prepare_index(-1)
+        print(index1, tm._tape[index1])
+        assert tm._tape[index1] == '1' and index1 == 0
+
+        index2 = tm._prepare_index(1)
+        print(index2, tm._tape[index2])
+        assert tm._tape[index2] == '3' and index2 == 2
+
+        index3 = tm._prepare_index(4)
+        print(index3, tm._tape[index3])
+        assert tm._tape[index3] == '' and index3 == 5
+
+        index4 = tm._prepare_index(-5)
+        print(index4, tm._tape[index4])
+        assert tm._tape[index4] == '' and index4 == 0
+
+
     def tear_down(self):
         s1 = str(self.tm)
         s2 = str(self.tm2)
