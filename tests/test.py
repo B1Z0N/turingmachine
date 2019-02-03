ddda72dd6dbd9a75e61ace6342aa24ab30f6fd3c
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-02-04 00:49:05 +0200

    Added temporary files support Fixed repr of a machine Refactored some code TODO: find the way to write paths(os module maybe)

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,7 @@ from turingmachine.machine import TuringMachine as tm
 from turingmachine.macro import TuringMachineMacro as tmm
 
 tm1 = tm.from_str('a,1,1,b,1,0,0,1,c:::q9:')
-t = tmm(tm1)
-t.bin_func('a', 'b', 'c', 'd', cond_gen='q')
-tm1.rule_file('temp.txt')
+tmac = tmm(tm1)
+tmac.bin_func('a', 'b', 'c', 'd', cond_gen='q')
 tm1.run()
-tm1.view()
+print(tm1)
