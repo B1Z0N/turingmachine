f548052d6bd21a4164a5e406fb808415d08aa889
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-02-05 20:00:02 +0200

    Added a lot of useful minifunctions to like copy or clean or put, to manage tape condition

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,11 @@
 from turingmachine.machine import TuringMachine as tm
 from turingmachine.macro import TuringMachineMacro as tmm
 
-tm1 = tm.from_str('a,1,1,b,1,0,0,1,c:::q1:')
+tm1 = tm.from_str('a,1,1,0,b,1,0,0,1,1,0,1,0,c:::q1:')
 tmac = tmm(tm1)
 tmac.bin_func('a', 'b', 'c', 'd')
 on_way = ['0', '1', 'b', 'c']
-tmac.move_by_val('d', 'R', on_way)
+tmac.move_one('c', on_way, 'R')
 tmac.stop()
 tm1.run()
 tm1.view()
