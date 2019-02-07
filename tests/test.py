67fb0a85c79011e39aca7f88f20daa9eb35624c5
B1Z0N <32279961+B1Z0N@users.noreply.github.com>
2019-02-07 13:47:00 +0200

    Fixed bugs in copy_range function Added move_range function

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,11 @@
 from turingmachine.machine import TuringMachine as tm
 from turingmachine.macro import TuringMachineMacro as tmm
 
-tm1 = tm.from_str('a,1,0,2,3,b,_,2,1,c,0,1,l,2,k,l,j,k:::q1:')
+tm1 = tm.from_str('a,1,0,b,1,0,1,1,_,c,0,1,l,2,k,l,j,1:::q1:')
 tmac = tmm(tm1)
-tmac.copy_range(
-    'a', ['1', '0', '2', '3'],
-    'b', ['_', '2', '1'],
+tmac.move_range(
+    'a', ['1', '0'],
+    'b', ['_', '0', '1'],
     'c', ['j', 'k', 'l', '1', '2', '0'],
     'R'
     )
